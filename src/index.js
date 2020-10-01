f4744e5cc1be5b25cccd6269f1d2bad5489eb895
Andreizayaz <poznavauka@mail.ru>
2020-10-01 21:39:52 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+exports.min = function min(array) {
+    // if(array===undefined) return 0;
+    if (array == null || array.length === 0) return 0;
 
-exports.min = function min (array) {
-  return 0;
+    let minNumber = array.sort((a, b) => a - b)[0];
+
+    return minNumber;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    // if(array===undefined) return 0;
+    if (array == null || array.length === 0) return 0;
+
+    let maxNumber = array.sort((a, b) => a - b)[array.length - 1];
+
+    return maxNumber;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    // if(array===undefined) return 0;
+    if (array == null || array.length === 0) return 0;
+
+    let avgResult = array.reduce((sum, current) => sum + current, 0) / array.length;
+
+    return avgResult;
 }
